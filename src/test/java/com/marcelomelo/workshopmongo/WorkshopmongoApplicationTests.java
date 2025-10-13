6dbf0372f57aa2dadd0a5c3b5829df35b3868106
Marcelo Melo <marcelovmelo201@gmail.com>
2025-10-13 16:21:30 -0300

    Projeto criado.

---
+++ src/test/java/com/marcelomelo/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcelomelo.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
